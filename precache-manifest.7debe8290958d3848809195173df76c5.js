b05e60ea52ad56bff9504d8b44671092547fb593
Shridhar-dev <shridharkamat10@gmail.com>
2020-05-03 19:29:08 +0530

    Updates

---
+++ precache-manifest.7debe8290958d3848809195173df76c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bd1b811804bbe7c471fb55e7c054a02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a2a4c26e1a6765947b3",
+    "url": "/static/css/main.60fea097.chunk.css"
+  },
+  {
+    "revision": "64175967c7ae464b354f",
+    "url": "/static/js/2.02bbbb38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.02bbbb38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a2a4c26e1a6765947b3",
+    "url": "/static/js/main.1187c2e0.chunk.js"
+  },
+  {
+    "revision": "53dde2945a0dbb99a519",
+    "url": "/static/js/runtime-main.89954229.js"
+  }
+]);
